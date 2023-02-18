7109d9b19adf7ffa8f637ca61713eebbe0e06c7d
Zerek <16066557+zerek-cheng@users.noreply.github.com>
2023-02-18 12:38:54 +0800

    备案信息修改

--- src/components/WebBottom.vue
+++ src/components/WebBottom.vue
@@ -1,7 +1,7 @@
 <template>
   <el-row id="footer">
     <el-col :span="24" id="footer-content">
-      <p>沪ICP备2022014867-1号 沪公网安备X号</p>
+      <p><a href="https://beian.miit.gov.cn/">沪ICP备2022014867-1号</a></p>
       <p>Powered By Vue 2.7.10 & ElementUi 2.15.9</p>
     </el-col>
   </el-row>
